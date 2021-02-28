59b4ea07641cadf8711661dbdc99fe4b60b58589
DARK-02 <76576010+DARK-02@users.noreply.github.com>
2021-02-28 10:08:47 +0700

    Create loding.py

---
+++ etc/loding.py
@@ -0,0 +1,19 @@
+from darkk.warn import *
+# -*- coding: utf-8 -*-
+def bnr():
+ print (f"""\033[1;36m
+   __                    __                                     __
+   \ \_____                             __
+###[==_____> dark                                                       __
+   /_/      __                  __                      __
+            \ \_____
+     scw  ###[==_____>                  __
+            /_/                                         __
+
+
+{hm}____________________________________________________________
+
+{dark}[$] BY > MR_DARK
+{hm}[$] DISCORD > https://discord.gg/YkcCjVMXre
+{dark}[$] Github > https://github.com/DARK-02                                                                                                                            {dark}[$] Youtube > MR_DARK
+{hm}____________________________________________________________""")
